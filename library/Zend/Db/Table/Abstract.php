088b7e38d43c80244b0591ca89db293b759ca756
makoto <makoto@fes-total.com>
2019-05-27 14:54:58 +0900

    最近のPHPだとエラーが出るため

--- library/Zend/Db/Table/Abstract.php
+++ library/Zend/Db/Table/Abstract.php
@@ -1304,7 +1304,7 @@ abstract class Zend_Db_Table_Abstract
         $whereList = array();
         $numberTerms = 0;
         foreach ($args as $keyPosition => $keyValues) {
-            $keyValuesCount = count($keyValues);
+            $keyValuesCount = is_array($keyValues) ? count($keyValues) : keyValues === null ? 0 : 1;
             // Coerce the values to an array.
             // Don't simply typecast to array, because the values
             // might be Zend_Db_Expr objects.
